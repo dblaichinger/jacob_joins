0825de0ab4b8183c584eb88cbbefcdf08baa31aa
Ismail Hanli <ismail.hanli@hotmail.com>
2012-03-12 18:59:07 +0100

    skeleton for static landing page

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'mongoid'
 gem 'bson_ext' # C extension to improve MongoDB performance
 gem 'mongoid_slug'
 gem 'mongoid-paperclip', :require => 'mongoid_paperclip'
+gem 'high_voltage'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/routes.rb
+++ config/routes.rb
@@ -67,7 +67,7 @@ JacobJoins::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'upload#recipe'
+   root :to => 'high_voltage/pages#show', :id => "index"
 
   # See how all your routes lay out with "rake routes"
 
