4dc97e9bce3d78de031d0690297a6e571cc4a8f2
Alexander Flatscher <alexander.flatscher@gmail.com>
2012-03-28 18:27:12 +0200

    testing, make it work

--- spec/support/factories/question_factory.rb
+++ spec/support/factories/question_factory.rb
@@ -2,4 +2,8 @@ FactoryGirl.define do
   factory :question do |f|
     f.text "How is the weather?"
   end
+
+  factory :another_question, :class => "Question" do |f|
+    f.text "How large is your country?"
+  end
 end
