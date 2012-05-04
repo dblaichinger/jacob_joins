9126e59a528961e6674ee0e6f934cebea2e701fe
Daniel Blaichinger <daniel.blaichi@gmx.at>
2012-05-04 18:28:18 +0200

    fb like button

--- app/assets/javascripts/news_bar.js
+++ app/assets/javascripts/news_bar.js
@@ -1,6 +1,5 @@
 function get_latest_recipe(){
   $.get('/recipes/last', function(data, textstatus, jqxhr) {
-
     $.each(data, function(key, recipe){
       var user_name;
       if(recipe.user_id != null){
@@ -145,11 +144,4 @@ function slide_newsbar(){
   });
 }
 
-//Facebook JS SDK
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+
