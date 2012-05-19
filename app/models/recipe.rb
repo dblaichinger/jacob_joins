2c81875dccfcedb74deaca9eb5c6df073c856661
Alexander Flatscher <alexander.flatscher@gmail.com>
2012-05-19 18:22:30 +0200

    rewrite recipe and step state_machine, tests

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -23,22 +23,25 @@ class Recipe
   embeds_many :ingredients_with_quantities
   accepts_nested_attributes_for :ingredients_with_quantities, :allow_destroy => true, :reject_if => :all_blank
 
-  embeds_many :steps, :validate => false, :cascade_callbacks => true
+  embeds_many :steps, :cascade_callbacks => true
   accepts_nested_attributes_for :steps, :allow_destroy => true, :reject_if => :all_blank
 
   embeds_many :images, :cascade_callbacks => true
   accepts_nested_attributes_for :images, :allow_destroy => true
   
   state_machine :initial => :draft do
+    before_transition :draft => :published, :do => :publish_embedded_steps
+    after_failure :on => :publish, :do => :unpublish_embedded_steps
+
     event :publish do
       transition :draft => :published
     end
 
     state :published do
-      validates_presence_of :name, :portions, :duration, :country, :latitude, :longitude
+      validates :name, :portions, :duration, :country, :latitude, :longitude, :presence => true
       validates :portions, :duration, :numericality => { :only_integer => true }
-      validates_associated :steps
     end
+
   end
 
   def formatted_portions
@@ -54,5 +57,13 @@ class Recipe
     ingredients_with_quantities.each do |ingredient_with_quantity|
       ingredients << Ingredient.find_or_create_by(:name => ingredient_with_quantity.name) if ingredients.where(:name => ingredient_with_quantity.name).count == 0
     end
+  end
+
+  def publish_embedded_steps
+    steps.all? { |step| step.publish }
+  end
+
+  def unpublish_embedded_steps
+    steps.where(:state => "published").each {|step| step.unpublish}
   end
 end
