950932f2c0f013bdc332dbcd5d1b4c99e15424cb
Marta Janosz <mjanosz.mma-b2009@fh-salzburg.ac.at>
2012-05-24 12:18:27 +0200

    about us

--- config/navigation.rb
+++ config/navigation.rb
@@ -1,7 +1,10 @@
 SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
     primary.item :home, 'Home', root_path, class:""
+    primary.item :story, 'Who is Jacob?', page_path("form")
     primary.item :form, 'Upload', "#{page_path("form")}#skipstory"
     primary.item :about, 'About us', page_path("about_us")
+    primary.item :search, 'Search', page_path("search")
+
   end
 end
