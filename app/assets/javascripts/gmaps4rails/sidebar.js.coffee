f5ecd3dd9f4f23e5d7efa835fd60dc3e681992d6
Daniel Blaichinger <daniel.blaichi@gmx.at>
2012-05-25 20:07:41 +0200

    Callback on marker click works

--- app/assets/javascripts/gmaps4rails/sidebar.js.coffee
+++ app/assets/javascripts/gmaps4rails/sidebar.js.coffee
@@ -8,6 +8,20 @@ window.switchSidebar = ->
       "margin-right": "0px",
       300
 
+window.showRecipeSidebar = (marker) ->
+  console.debug(marker)
+
+
+  $.ajax 
+    url: "/recipes/getSidebar"
+    data: {"marker": marker.description, "type": "getSearchSidebar"}
+    dataType: "json"
+    type: "GET"
+    success: (data, textStatus, jqXHR) ->
+      console.debug(data)
+    error: (jqXHR, textStatus, errorThrown) ->
+      console.debug(errorThrown)
+
 
 #Gmaps4RailsGoogle.event.addListener marker, "click", ->
 #  alert "works"
--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,8 @@ JacobJoins::Application.routes.draw do
   match 'recipes/delete_step_image/:step_id' => 'recipes#delete_step_image', :via => :delete, :as => 'recipes_delete_step_image'
   match 'recipes/upload_image' => 'recipes#upload_image', :via => :post
   match 'recipes/delete_image/:image_id' => 'recipes#delete_image', :via => :delete, :as => 'recipes_delete_image'
-  match '/recipes/search' => 'recipes#search', :via => [:get, :post]
+  match 'recipes/search' => 'recipes#search', :via => [:get, :post]
+  match 'recipes/getSidebar' => 'recipes#getSidebar', :via => [:get, :post]
 
   match 'country_specific_informations/sync_wizard' => 'country_specific_informations#sync_wizard', :via => [:post, :put]
   match 'users/sync_wizard' => 'users#sync_wizard', :via => [:post, :put]
