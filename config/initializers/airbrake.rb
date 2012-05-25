5139155e4b7607e6300f16cd0d387d460d4695eb
Ismail Hanli <ismail.hanli@hotmail.com>
2012-05-25 11:44:22 +0200

    add test environment to development environments of airbrake

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,4 +1,4 @@
 Airbrake.configure do |config|
   config.api_key = '444a2d3ad19924ae884bc77ec39766c5'
-  config.development_environments = ["development"]
+  config.development_environments = ["development", "test"]
 end
